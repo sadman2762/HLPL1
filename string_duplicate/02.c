7b69cf59bb237312606f23aa2914a831edd1cf47
sadman2762 <smsadmansakibsayor.gmail.com>
2024-03-29 14:57:56 +0100

    a

---
+++ string_duplicate/02.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+char* double_consonants(char* origin){
+    char* result=(char*)calloc(strlen(origin)*2+1,sizeof(char));
+
+    int pos=0;
+    for(int i=0; i<strlen(origin);i++){
+        if(strchr("bcdfghjklmnpqrstvwxyz",tolower(origin[i]))){
+            result[pos++] = origin[i];
+        }
+        result[pos++]=origin[i];
+
+    }
+    return result;
+}
+
+int main(){
+    char line[101];
+
+    while(gets(line)){
+        char* result=double_consonants(line);
+        puts(result);
+        free(result);
+    }
+    return EXIT_SUCCESS;
+}
