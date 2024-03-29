9d4596c7f62348f317a2f10bea3f71811f9a6349
sadman2762 <smsadmansakibsayor.gmail.com>
2024-03-29 08:41:12 +0100

    niyat

---
+++ string_remove/06.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+char* delete_digits(char* origin){
+    char* result= (char*)calloc(strlen(origin)+1,sizeof(char));
+
+    int pos = 0;
+    for(int i = 0; i < strlen(origin); i++){
+        if(!isdigit(origin[i])){
+            result[pos++] = origin[i];
+        }
+    }
+    return result;
+}
+
+int main(){
+    char line[101];
+
+    while(1){
+        gets(line);
+        if(strcmp(line,"")== 0){
+            break;
+        }
+        char* result= delete_digits(line);
+        puts(result);
+        free(result);
+    }
+    return EXIT_SUCCESS;
+}
---
+++ string_remove/08.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+char* delete_even_digits(char* origin){
+    char* result= (char*)calloc(strlen(origin)+1, sizeof(char));
+
+    int pos=0;
+    for(int i=0; i<strlen(origin);i++){
+        if(!(isdigit(origin[i]) && (origin[i]-'0')%2 == 0)){
+            result[pos++] = origin[i];
+        }
+    }
+    return result;
+}
+
+int main(){
+    char line[101];
+
+    while(gets(line)){
+        char *result = delete_even_digits(line);
+        puts(result);
+        free(result);
+    }
+
+    return EXIT_SUCCESS;
+}
