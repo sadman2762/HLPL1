c26e91bcda35f0b9d1395c7eaa33d0b8a42bc392
sadman2762 <smsadmansakibsayor.gmail.com>
2024-03-29 13:10:24 +0100

    niyat

---
+++ string_remove/18.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+char* delete_spaces(char* origin){
+    char* result = (char*)calloc(strlen(origin)+1,sizeof(char));
+
+    int pos=0;
+    for(int i=0; i<strlen(origin); i++){
+        if(origin[i] != ' ' ){
+            result[pos++] = origin[i];
+        }
+    }
+    return result;
+}
+
+int main(){
+    char line[101];
+
+    while(1){
+        gets(line);
+        if(strcmp(line,"")==0){
+            break;
+        }
+        char* result= delete_spaces(line);
+        puts(result);
+        free(result);
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ string_remove/21.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+char* delete_uppers(char* origin){
+    char* result= (char*)calloc(strlen(origin)+1,sizeof(char));
+
+    int pos=0;
+    for(int i=0;i<strlen(origin);i++){
+        if(!isupper(origin[i])){
+            result[pos++]=origin[i];
+        }
+    }
+    return result;
+}
+
+int main(){
+    char line[101];
+
+    while(1){
+        gets(line);
+        if(strcmp(line,"")==0){
+            break;
+        }
+        char* result=delete_uppers(line);
+        puts(result);
+        free(result);
+    }
+
+    return EXIT_SUCCESS;
+}
