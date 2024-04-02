3fa56a9bcd7603dd4b85972259c0dc88345ab283
sadman2762 <smsadmansakibsayor.gmail.com>
2024-04-02 20:38:23 +0200

    a

---
+++ array_validate_new/13.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int is_strictly_monotone_decreasing(int* numbers,int length){
+    for(int i=1;i<length;i++){
+        if(numbers[i] >= numbers[i-1]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        int length;
+        scanf("%d",&length);
+        int numbers[length];
+
+        for(int j=0;j<length;j++){
+            scanf("%d",&numbers[j]);
+        }
+        int result = is_strictly_monotone_decreasing(numbers,length);
+        printf("%s\n",result?"True":"False");
+    }
+    return EXIT_SUCCESS;
+}
---
+++ array_validate_new/18.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int is_strictly_monotone_increasing(int* numbers, int length){
+    for(int i=1;i<length;i++){
+        if(numbers[i]<=numbers[i-1]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(){
+    while (1)
+    {
+        int n;
+        scanf("%d",&n);
+        if(n==0){
+            break;
+        }
+        int numbers[n];
+        for(int i=0;i<n;i++){
+            scanf("%d",&numbers[i]);
+        }
+        int result=is_strictly_monotone_increasing(numbers,n);
+        printf("%s\n",result?"True":"False");
+    }
+    return EXIT_SUCCESS;
+    
+}
